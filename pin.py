df1fcdf0262a381dddb21c43d3ee1d752d12ff17
drforse <46328218+drforse@users.noreply.github.com>
2019-05-16 17:30:27 +0100

    pin_members_right

--- pin.py
+++ pin.py
@@ -23,41 +23,48 @@ def check(message):
 @bot.message_handler(commands = ['pintime'])
 def pintime(message):
     try:
+        chat_member = bot.get_chat_member(message.chat.id, message.from_user.id)
         quant = 3
         if message.chat.type == 'private':
             bot.send_message(message.chat.id, 'Only for groups')
         elif message.reply_to_message == None:
             bot.send_message(message.chat.id, 'make replay')
-        elif message.text in ['/pintime', '/pintime@botsdaddyybot']:
-            while quant > 0:
-                try:
-                    bot.unpin_chat_message(message.chat.id)
-                    bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id)
-                except Exception:
-                    bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id)
-                quant -= 1
-                time.sleep(3)
+        elif chat_member.can_pin_messages == True:
+            if message.text in ['/pintime', '/pintime@botsdaddyybot']:
+                while quant > 0:
+                    try:
+                        bot.unpin_chat_message(message.chat.id)
+                        bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id)
+                    except Exception:
+                        bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id)
+                    quant -= 1
+                    time.sleep(3)
+            else:
+                arg = message.text.split(' ')
+                quant = int(arg[1])
+                while quant > 0:
+                    try:
+                        bot.unpin_chat_message(message.chat.id)
+                        bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id)
+                    except Exception:
+                        bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id)
+                    quant -= 1
+                    time.sleep(3)
         else:
-            arg = message.text.split(' ')
-            quant = int(arg[1])
-            while quant > 0:
-                try:
-                    bot.unpin_chat_message(message.chat.id)
-                    bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id)
-                except Exception:
-                    bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id)
-                quant -= 1
-                time.sleep(3)
+            bot.send_message(message.chat.id, 'У тебя нет пинилки', reply_to_message_id = message.message_id)
     except Exception:
         bot.send_message(message.chat.id, traceback.format_exc())
 
 @bot.message_handler(commands = ['pinn'])
 def pin(message):
     try:
+        chat_member = bot.get_chat_member(message.chat.id, message.from_user)
         if message.chat.type == 'private':
             bot.send_message(message.chat.id, 'Only for groups')
         elif message.reply_to_message == None:
-            bot.send_message(message.chat.id, 'make replay')
+            bot.send_message(message.chat.id, 'make replay', reply_to_message_id = message.message_id)
+        elif chat_member.can_pin_messages == False:
+            bot.send_message(message.chat.id, 'У тебя нет пинилки', reply_to_message_id = message.message_id)
         else:
             bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id)
     except Exception:
@@ -66,10 +73,13 @@ def pin(message):
 @bot.message_handler(commands = ['pin'])
 def pin_silent(message):
     try:
+        chat_member = bot.get_chat_member(message.chat.id, message.from_user)
         if message.chat.type == 'private':
             bot.send_message(message.chat.id, 'Only for groups')
         elif message.reply_to_message == None:
-            bot.send_message(message.chat.id, 'make replay')
+            bot.send_message(message.chat.id, 'make replay', reply_to_message_id = message.message_id)
+        elif chat_member.can_pin_messages == False:
+            bot.send_message(message.chat.id, 'У тебя нет пинилки', reply_to_message_id = message.message_id)
         else:
             bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id, True)
     except Exception:
@@ -78,10 +88,13 @@ def pin_silent(message):
 @bot.message_handler(commands = ['unpin'])
 def unpin(message):
     try:
+        chat_member = bot.get_chat_member(message.chat.id, message.from_user)
         if message.chat.type == 'private':
-            bot.send_message(message.chat.id, 'Only for groups')
+            bot.send_message(message.chat.id, 'Only for groups', reply_to_message_id = message.message_id)
         elif message.reply_to_message == None:
             bot.unpin_chat_message(message.chat.id, message.message_id)
+        elif chat_member.can_pin_messages == False:
+            bot.send_message(message.chat.id, 'У тебя нет пинилки', reply_to_message_id = message.message_id)
         else:
             bot.unpin_chat_message(message.chat.id, message.reply_to_message.message_id)
     except Exception:
