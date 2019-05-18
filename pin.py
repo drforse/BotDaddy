19b3f9bc87431be2cdc2290c3ec0707115dcdb33
drforse <46328218+drforse@users.noreply.github.com>
2019-05-19 00:24:08 +0100

    Update pin.py

--- pin.py
+++ pin.py
@@ -14,8 +14,10 @@ posts = db.posts
 
 bot = telebot.TeleBot (os.environ['token'])
 
-ban_keywords_list = ['иди в баню','иди в бан','банан тебе в жопу','нам будет тебя не хватать', '/ban']
-unban_keywords_list = ['мы скучаем', 'выходи из бани', 'кончил', '/unban']
+ban_keywords_list = ['!иди в баню','!иди в бан','!банан тебе в жопу','!нам будет тебя не хватать', '/ban']
+unban_keywords_list = ['!мы скучаем', '!выходи из бани', '!кончил', '/unban']
+mute_keywords_list = ['!мут']
+unmute_keywords_list = ['!анмут']
 
 @bot.message_handler(commands = ['chat_id'])
 def chat_id(message):
@@ -124,21 +126,24 @@ def get_pinned_messages(message):
         else:
             bot.send_message(message.from_user.id, text, parse_mode = 'html', disable_web_page_preview = True)
         bot.send_message(message.chat.id, 'Отправил тебе в лс')
-    except:
+    except Exception:
         bot.send_message(message.chat.id, traceback.format_exc())
     
 @bot.message_handler(content_types = ['text'])
-def ban(message):
+def ban_mute(message):
+#ban
     try:
         chat_member = bot.get_chat_member(message.chat.id, message.from_user.id)
         if message.text.lower() in ban_keywords_list:
             if chat_member.can_restrict_members == True or chat_member.status == 'creator':
                 bot.kick_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+                bot.send_message(message.chat.id, '<a href="tg://user?id="{}">{}</a> забанен, вините во всем Путина!'.format(message.reply_to_message.from_user.id, message.reply_to_message.from_user.first_name), parse_mode = 'html')
             else:
                 bot.send_message(message.chat.id, 'у тебя нет банилки',reply_to_message_id = message.message_id)
         if message.text.lower() in unban_keywords_list:
             if chat_member.can_restrict_members == True or chat_member.status == 'creator':
                 bot.unban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+                bot.send_message(message.chat.id, '<a href="tg://user?id="{}">{}</a> разбанен!'.format(message.reply_to_message.from_user.id, message.reply_to_message.from_user.first_name), parse_mode = 'html')
             else:
                 bot.send_message(message.chat.id, 'у тебя нет банилки',reply_to_message_id = message.message_id)
         if message.text.lower() == 'бан':
@@ -150,7 +155,26 @@ def ban(message):
         bot.send_message(message.chat.id, 'make reply', reply_to_message_id = message.message_id)
     except Exception:
         bot.send_message(message.chat.id, traceback.format_exc())   
-        
+#mute
+    try:
+        chat_member = bot.get_chat_member(message.chat.id, message.from_user.id)
+        if message.text.lower() in mute_keywords_list:
+            if chat_member.can_restrict_members == True or chat_member.status == 'creator':
+                bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+                bot.send_message(message.chat.id, '<a href="tg://user?id="{}">{}</a> был брошен в мут!'.format(message.reply_to_message.from_user.id, message.reply_to_message.from_user.first_name), parse_mode = 'html')
+            else:
+                bot.send_message(message.chat.id, 'У тебя нет таких прав, холоп!',reply_to_message_id = message.message_id)
+        if message.text.lower() in unmute_keywords_list:
+            if chat_member.can_restrict_members == True or chat_member.status == 'creator':
+                bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+                bot.send_message(message.chat.id, '<a href="tg://user?id="{}">{}</a> был вызволен из мута!'.format(message.reply_to_message.from_user.id, message.reply_to_message.from_user.first_name), parse_mode = 'html')
+            else:
+                bot.send_message(message.chat.id, 'У тебя нет таких прав, холоп!',reply_to_message_id = message.message_id)
+    except AttributeError:
+        bot.send_message(message.chat.id, 'make reply', reply_to_message_id = message.message_id)
+    except Exception:
+        bot.send_message(message.chat.id, traceback.format_exc())   
+    
 @bot.message_handler(content_types = ['pinned_message'])
 def store_pinned_messages(message):
     try:
@@ -169,4 +193,3 @@ def store_pinned_messages(message):
         bot.send_message(message.chat.id, traceback.format_exc())
         
 bot.polling()
-
