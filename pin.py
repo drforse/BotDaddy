0ed45288ef171133e1454e42853f5a886ecb7085
drforse <46328218+drforse@users.noreply.github.com>
2019-06-28 14:53:35 +0100

    Update pin.py

--- pin.py
+++ pin.py
@@ -158,11 +158,11 @@ def chat_id(message):
 @bot.message_handler(commands = ['user'])
 def user_info(m):
     if m.reply_to_message:
-        bot.send_message(m.chat.id, '{}\n{}\n`{}`'.format(m.reply_to_message.from_user.first_name, m.reply_to_message.from_user.username, m.reply_to_message.from_user.id), parse_mode = 'markdown')
+        bot.send_message(m.chat.id, '{}\n{}\n<code>{}</code>'.format(m.reply_to_message.from_user.first_name, m.reply_to_message.from_user.username, m.reply_to_message.from_user.id), parse_mode = 'html')
     elif len(m.text.split()>1):
         try:
             member = bot.get_chat_member(m.chat.id, m.text.split()[1])
-            bot.send_message(m.chat.id, '{}\n{}\n`{}`'.format(member.user.first_name, member.user.username, member.user.id), parse_mode = 'markdown')
+            bot.send_message(m.chat.id, '{}\n{}\n<code>{}</code>`'.format(member.user.first_name, member.user.username, member.user.id), parse_mode = 'html')
         except:
             bot.send_message(m.chat.id, 'Аргументы неверны')
     
