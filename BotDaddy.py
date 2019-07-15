40b8376e4ee56bba73a25a3770a3dd3a33da9e0c
drforse <46328218+drforse@users.noreply.github.com>
2019-07-15 22:09:38 +0100

    Add files via upload

--- BotDaddy.py
+++ BotDaddy.py
@@ -8,6 +8,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.utils.executor import start_webhook
 import os
 import aiocron
 import time
