d18b2ad00c6948d2c87d8779e4a9ba5e01e1b51c
drforse <george.lifeslice@gmail.com>
2019-08-11 14:41:51 +0100

    added support for forwarded_messages in winrate

--- BotDaddy.py
+++ BotDaddy.py
@@ -724,17 +724,17 @@ async def set_auto_hangstats(m):
 async def send_winrate(m):
     try:
         if m.reply_to_message and m.reply_to_message.text:
-            if len(m.reply_to_message.text.split(':')) == 3 and\
-                    m.reply_to_message.text.split(':')[1].split()[0].isdigit()\
-                    and m.reply_to_message.text.split(':')[2].split()[0].isdigit()\
-                    and m.reply_to_message.from_user.id == 121913006:
-                wins = int(m.reply_to_message.text.split(':')[1].split()[0])
-                loses = int(m.reply_to_message.text.split(':')[2].split()[0])
-            elif m.reply_to_message.from_user.id == 443471829\
-                    and len(m.reply_to_message.text.split('.')) == 4 and len(m.reply_to_message.text.split(':')) == 2\
-                    and len(m.reply_to_message.text.split('\n')) == 4:
-                wins = int(m.reply_to_message.text.split('\n')[2].split()[0])
-                loses = int(m.reply_to_message.text.split('\n')[1].split()[0]) - wins
+            if m.reply_to_message.from_user.id == 121913006 or m.reply_to_message.forward_from.id == 121913006:
+                if len(m.reply_to_message.text.split(':')) == 3 and\
+                        m.reply_to_message.text.split(':')[1].split()[0].isdigit()\
+                        and m.reply_to_message.text.split(':')[2].split()[0].isdigit():
+                    wins = int(m.reply_to_message.text.split(':')[1].split()[0])
+                    loses = int(m.reply_to_message.text.split(':')[2].split()[0])
+            elif m.reply_to_message.from_user.id == 443471829 or m.reply_to_message.forward_from.id == 443471829:
+                if len(m.reply_to_message.text.split('.')) == 4 and len(m.reply_to_message.text.split(':')) == 2\
+                        and len(m.reply_to_message.text.split('\n')) == 4:
+                    wins = int(m.reply_to_message.text.split('\n')[2].split()[0])
+                    loses = int(m.reply_to_message.text.split('\n')[1].split()[0]) - wins
             winrate = await get_hangbot_winrate(wins, loses)
             await bot.send_message(m.chat.id, f'Winrate: ~{winrate} %', reply_to_message_id=m.message_id)
     except:
