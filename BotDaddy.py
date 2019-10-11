0a2b6697391004676196edc462315a585f16a379
drforse <george.lifeslice@gmail.com>
2019-10-12 00:19:35 +0100

    back to polling
    I JUST DON'T KNOW HOW TO MAKE HEROKU DO NOT IDLE WITH WEBHOOKS

--- BotDaddy.py
+++ BotDaddy.py
@@ -31,10 +31,12 @@ import threading
 
 API_TOKEN = os.environ['token']
 
+'''
 WEBHOOK_HOST = os.environ['heroku_app']
 WEBHOOK_PORT = 443
 WEBHOOK_PATH = '/path/to/api'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+'''
 
 logging.basicConfig(level=logging.WARNING)
 loop = asyncio.get_event_loop()
@@ -1172,9 +1174,5 @@ async def on_startup(dp):
 async def on_shutdown(dp):
     pass
 
-def anti_idling():
-    while True:
-        asyncio.run(asyncio.sleep(60*60*24*31))
-threading.Thread(target=anti_idling).start()
 
-start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH, on_startup=on_startup, on_shutdown=on_shutdown, skip_updates=True, host='0.0.0.0', port=os.getenv('PORT'))
+executor.start_polling(dp, loop=loop, skip_updates=True)
