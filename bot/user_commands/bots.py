d148f7813a5ff7bed721e0cc932632b748380fe8
dr_forse <george.lifeslice@gmail.com>
2020-04-04 16:54:06 +0100

    few fixes

--- bot/user_commands/bots.py
+++ bot/user_commands/bots.py
@@ -13,6 +13,10 @@ class Bots(Command):
 
     @classmethod
     async def execute(cls, m: Message):
+        if m.chat.type == 'private':
+            me = await bot.get_me()
+            await bot.send_message(m.chat.id, f'{me.first_name} (@{me.username})')
+            return
         generator = telethon_bot.iter_participants(m.chat.id, filter=ChannelParticipantsBots)
         bots = [bot_ async for bot_ in generator]
         msg_text = ''.join([f'{bot_.first_name} (@{bot_.username})\n' for bot_ in bots])
