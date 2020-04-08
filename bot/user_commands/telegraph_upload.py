75b66a064dab2a550427b9ee9429c4f5f3fb52b1
dr_forse <george.lifeslice@gmail.com>
2020-04-08 17:34:31 +0100

    move from telegraph to aiograph

--- bot/user_commands/telegraph_upload.py
+++ bot/user_commands/telegraph_upload.py
@@ -2,12 +2,11 @@ from config import bot
 from aiogram.types import Message, PhotoSize
 from aiogram import exceptions as aioexcs
 from ..core import Command
-import requests
 import typing
-import pathlib
-from telegraph.upload import upload_file
-import os
+from aiograph import Telegraph
 import uuid
+
+telegraph = Telegraph()
 
 
 class TelegraphUpload(Command):
@@ -19,40 +18,35 @@ class TelegraphUpload(Command):
 
     @classmethod
     async def execute(cls, m: Message):
-        await bot.send_chat_action(m.chat.id, 'upload_photo')
+        await bot.send_chat_action(m.chat.id, 'typing')
         if m.reply_to_message and 'url' in [i.type for i in m.reply_to_message.entities]:
-            files = [i async for i in cls.proccess_photos(m.reply_to_message.text.splitlines())]
+            urls = [i async for i in cls.proccess_photos(m.reply_to_message.text.splitlines())]
         elif m.reply_to_message and m.reply_to_message.photo:
-            files = [await cls._proccess_photo(m.reply_to_message.photo[-1])]
+            urls = [await cls._proccess_photo(m.reply_to_message.photo[-1])]
         elif len(m.text.split()) > 1 and 'url' in [i.type for i in m.entities]:
-            files = [i async for i in cls.proccess_photos(m.text.split()[1:])]
+            urls = [i async for i in cls.proccess_photos(m.text.split()[1:])]
         else:
             await bot.send_message(m.chat.id, 'No photos or uls passed')
             return
-        await bot.send_chat_action(m.chat.id, 'upload_photo')
-        urls = upload_file(files)
-        urls = ['https://telegra.ph%s' % url for url in urls]
         try:
             await bot.send_message(m.chat.id, '\n'.join(urls), reply_to_message_id=m.message_id)
         except aioexcs.MessageToReplyNotFound:
             await bot.send_message(m.chat.id, '\n'.join(urls))
-        for f in files:
-            os.remove(f)
 
     @classmethod
     async def proccess_photos(cls, photos: typing.Iterable[typing.Union[str, PhotoSize]])\
-            -> typing.AsyncGenerator[pathlib.Path, None]:
+            -> typing.AsyncGenerator[str, None]:
         for photo in photos:
             yield await cls._proccess_photo(photo)
 
     @staticmethod
-    async def _proccess_photo(photo: typing.Union[str, PhotoSize]) -> pathlib.Path:
+    async def _proccess_photo(photo: typing.Union[str, PhotoSize]) -> str:
         if type(photo) == PhotoSize:
-            path = str(photo.file_id)
-            await photo.download(path)
-            return pathlib.Path.cwd() / pathlib.Path(path)
-        r = requests.get(photo)
+            url = await photo.get_url()
+        elif photo.replace('https://', '').replace('www.', '').startswith('telegra.ph'):
+            return photo
+        else:
+            url = photo
         name = str(uuid.uuid4())
-        with open(name, 'wb') as f:
-            f.write(r.content)
-        return pathlib.Path.cwd() / pathlib.Path(name)
+        uploaded = await telegraph.upload_from_url(url=url, filename=name)
+        return uploaded
