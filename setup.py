99847b7f0cfc6a1e18215e2086ecae3dc2314a82
drforse <george.lifeslice@gmail.com>
2020-09-12 16:45:32 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setuptools.setup(
     name="bot_daddy_bot",
-    version="2.0.0",
+    version="2.0.1",
     author="drforse",
     author_email="george.lifeslice@gmail.com",
     description="Telegram bot with many functions",
