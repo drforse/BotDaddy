70b139f39634d96ca81ffaad42620896ccf158a4
drforse <george.lifeslice@gmail.com>
2020-12-23 04:33:47 +0000

    get_first_msg -> add support for messages from channels and groups, cache results in database

--- bot_daddy_bot/bot/user_commands/get_first_msg.py
+++ bot_daddy_bot/bot/user_commands/get_first_msg.py
@@ -1,4 +1,6 @@
-from ...config import bot
+import traceback
+
+from ...config import bot, col_groups_users
 from aiogram.types import Message
 from aiogram import exceptions as tg_excs
 from telethon import errors as telethon_errors
@@ -15,26 +17,53 @@ class GetFirstMsg(Command):
 
     @classmethod
     async def execute(cls, m: Message):
-        try:
-            if not m.reply_to_message:
-                await bot.send_message(chat_id=m.chat.id, text='Отправьте команду реплаем!')
-                return
+        if not m.reply_to_message:
+            await bot.send_message(chat_id=m.chat.id, text='Отправьте команду реплаем!')
+            return
 
-            first_message = FirstMessage(msg=m.reply_to_message)
-            link = await first_message.get_link()
-            mid = await first_message.get_id()
-            try:
-                await bot.forward_message(chat_id=m.chat.id, from_chat_id=m.chat.id, message_id=mid)
-            except tg_excs.BadRequest:
-                pass
-            await bot.send_message(chat_id=m.chat.id, text=link, reply_to_message_id=m.message_id)
-        except (ValueError, telethon_errors.rpcerrorlist.ChannelPrivateError):
+        rpl = m.reply_to_message
+        from_id = rpl.sender_chat.id if rpl.sender_chat else rpl.from_user.id
+        group_doc = col_groups_users.find_one({
+            "group": m.chat.id,
+            f"first_message_ids.{from_id}": {"$exists": True}
+        })
+        mid, link = 0, ""
+        if group_doc and "--clear" not in m.get_args():
+            mid = group_doc["first_message_ids"][str(from_id)]
+            if m.chat.username:
+                link = f't.me/{m.chat.username}/{mid}'
+            else:
+                link = f't.me/c/{m.chat.id}/{mid}'.replace("-100", "")
+
+        try:
+            if not mid:
+                first_message = FirstMessage(msg=rpl)
+                link = await first_message.get_link()
+                mid = await first_message.get_id()
+        except (ValueError, AttributeError, telethon_errors.rpcerrorlist.ChannelPrivateError):
+            traceback.print_exc()
             await bot.send_message(chat_id=m.chat.id,
                                    text='Добавьте в чат @P1voknopka и откройте историю сообщений для '
                                         'новых пользователей или сделайте чат публичным :/')
+            return
         except telethon_errors.rpcerrorlist.InputUserDeactivatedError:
             await bot.send_message(chat_id=m.chat.id, text='С удаленными пользователями не получится :/')
-        except AttributeError:
-            await bot.send_message(chat_id=m.chat.id,
-                                   text='Добавьте в чат @P1voknopka и откройте историю сообщений для '
-                                        'новых пользователей или сделайте чат публичным :/')
+            return
+
+        try:
+            await bot.forward_message(chat_id=m.chat.id, from_chat_id=m.chat.id, message_id=mid)
+        except tg_excs.BadRequest:
+            pass
+
+        await bot.send_message(
+            chat_id=m.chat.id,
+            text=link,
+            reply_to_message_id=m.message_id,
+            allow_sending_without_reply=True
+        )
+
+        if not group_doc:
+            col_groups_users.update_one({
+                "group": m.chat.id}, {
+                "$set": {f"first_message_ids.{from_id}": mid}
+            }, upsert=True)
--- bot_daddy_bot/userbot/userbot.py
+++ bot_daddy_bot/userbot/userbot.py
@@ -1,31 +1,61 @@
+from aiogram.types import Message as BotApiMessage
+from telethon.tl.types import Message
+
 from ..mixin_types import TelethonClient
 
 
 class FirstMessage:
-    def __init__(self, msg):
+    def __init__(self, msg: BotApiMessage):
         self.m = msg
+        self._message = None
         self.link = None
 
     async def get_link(self):
-        client = TelethonClient.get_current()
-        async with client:
-            chat = self.m.chat.username or self.m.chat.id
-            m = await client.get_messages(entity=chat, ids=[self.m.message_id])
-            m = m[0]
-            first_msg = await client.get_messages(entity=chat, from_user=m.from_id, limit=1, reverse=True)
-            first_msg = first_msg[0]
-            if m.chat.username:
-                self.link = f't.me/{m.chat.username}/{first_msg.id}'
-            else:
-                self.link = f't.me/c/{m.chat.id}/{first_msg.id}'
-            return self.link
+        m = await self.message
+        if m.chat.username:
+            self.link = f't.me/{m.chat.username}/{m.id}'
+        else:
+            self.link = f't.me/c/{m.chat_id}/{m.id}'.replace("-100", "")
+        return self.link
 
     async def get_id(self):
+        """left for backward compatibility after creating message property"""
+        return (await self.message).id
+
+    @property
+    async def message(self) -> Message:
+        """
+        gets the first message
+        in case if message is sent by channel or group -> retrieves all messages and does filtering here
+        in case if message is sent by user -> search request (from_user=m.from_user.id), so filtering is on telegram side
+        it's made different because didn't work with from_user==channel_id/group_id neither 1087968824 or 777000
+        """
+        if self._message:
+            return self._message
         client = TelethonClient.get_current()
         async with client:
             chat = self.m.chat.username or self.m.chat.id
-            m = await client.get_messages(entity=chat, ids=[self.m.message_id])
-            m = m[0]
-            first_msg = await client.get_messages(entity=chat, from_user=m.from_id, limit=1, reverse=True)
+            if self.m.sender_chat:
+                messages = client.iter_messages(
+                    entity=chat,
+                    reverse=True,
+                )
+                async for msg in messages:
+                    if msg.sender_id == self.m.sender_chat.id:
+                        self._message = msg
+                        return msg
+                    elif self.m.from_user.id == 1087968824 and msg.sender_id is None:
+                        return msg
+
+            # needed for getting sender's input entity
+            await client.get_messages(entity=chat, ids=[self.m.message_id])
+
+            first_msg = await client.get_messages(
+                entity=chat,
+                from_user=self.m.from_user.id,
+                limit=1,
+                reverse=True
+            )
             first_msg = first_msg[0]
-            return first_msg.id
+            self._message = first_msg
+            return self._message
