402efa4c6bdcff9e0655c27f6cd54bc43787ea71
drforse <george.lifeslice@gmail.com>
2020-12-28 00:39:01 +0000

    fix and improve several emojis processing; only process unique emojis

--- bot_daddy_bot/bot/other_handlers/emoji.py
+++ bot_daddy_bot/bot/other_handlers/emoji.py
@@ -10,22 +10,34 @@ EMOJI_REGEXP = emoji.get_emoji_regexp()
 
 class Emoji(Command):
 
+    TOO_MUCH_EMOJIS_SERVICE_MESSAGE = ("\n<i>Only first {n} emojis are used because "
+                                       "of message length limit and no desire of "
+                                       "sending several long messages, I don't really see any sense "
+                                       "in checking so much emojis through bot lol</i>")
+
     @classmethod
     async def execute(cls, m: types.Message, state: FSMContext = None, **kwargs):
-        for emo in emoji.emoji_lis(m.text or m.sticker.emoji):
-            em = emo["emoji"]
-            text_code = emoji.demojize(em)
-            alias_text_code = emoji.demojize(em, use_aliases=True)
+        s = ""
+        n = 0
+        emojis = set([e["emoji"] for e in emoji.emoji_lis(m.text or m.sticker.emoji)])
+        for emo in emojis:
+            text_code = emoji.demojize(emo)
+            alias_text_code = emoji.demojize(emo, use_aliases=True)
 
             # :X converts to hexadecimal
-            unicode = f"0+{ord(em):X}"
+            unicode = f"0+{ord(emo):X}"
+            append = (f"<b>Info about {emo}</b>\n"
+                      f"<code>{alias_text_code}</code>\n"
+                      f"<code>{text_code}</code>\n"
+                      f"<code>{unicode}</code>\n\n")
+            if len(s + append) > 4096 - len(cls.TOO_MUCH_EMOJIS_SERVICE_MESSAGE.format(n=n)):
+                s += cls.TOO_MUCH_EMOJIS_SERVICE_MESSAGE.format(n=n)
+                break
+            s += append
+            n += 1
 
-            s = (f"<b>Info about {em}</b>\n"
-                 f"<code>{alias_text_code}</code>\n"
-                 f"<code>{text_code}</code>\n"
-                 f"<code>{unicode}</code>")
-            await m.answer(s, parse_mode='html')
+        await m.answer(s, parse_mode='html')
 
-            # needed for continuing to the passive text handler
-            if m.text:
-                raise SkipHandler
+        # needed for continuing to the passive text handler
+        if m.text:
+            raise SkipHandler
